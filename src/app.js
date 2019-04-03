03596cc6f2526d118457842a2f27044f6d2e1416
Henry <hpc3@njit.edu>
2019-04-03 19:46:38 -0400

    Task: Moved top level component over to app.js, added API information

---
+++ src/app.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: '',
+            tasks: []
+        };
+    }
+
+
+    componentDidMount() {
+        this.getData();
+    }
+
+
+
+    getData(){
+        axios.get('http://my-json-server.typicode.com/hpc3/project2JSONDB/tasks')
+            .then(response =>{
+                this.setState({tasks: response.data});
+            }).catch(error => {
+            this.setState({errorMessage: error.message });
+        });
+    }
+
+
+
+
+    onChange = (event) => {
+        this.setState({ term: event.target.value });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            term: '',
+            items: [...this.state.items, this.state.term]
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="App" onSubmit={this.onSubmit}>
+                    <input value={this.state.term} onChange={this.onChange} />
+                    <button>Submit</button>
+                </form>
+                {/*<List items={this.state.items} />*/}
+            </div>
+        );
+    }
+}
+
+export default App;
